908684120ee51455cc83717042d4a30dff4d7cd4
bitocde <marcdrozrosario@gmail.com>
2023-05-09 14:39:21 -0500

    modified the on_select function and the M.metacode_ai_picker function to prevent the window from closing and getting the API result in the answer window

--- lua/metacode_ai/init.lua
+++ lua/metacode_ai/init.lua
@@ -26,14 +26,14 @@ local function get_user_question(on_done, opts)
     },
     sorter = conf.generic_sorter(opts),
     attach_mappings = function(prompt_bufnr, map)
-	local function on_select()
+
+    local function on_select(prompt_bufnr, on_done)
   local selection = action_state.get_selected_entry()
   if not selection then
-    -- If there is no selection, close the prompt and return
-    actions.close(prompt_bufnr)
+    -- If there is no selection, just return
     return
   end
-  actions.close(prompt_bufnr)
+  -- Call on_done with the selected value without closing the window
   on_done(selection.value)
 end
 
@@ -53,31 +53,50 @@ end
 function M.metacode_ai_picker(opts)
   opts = opts or {}
 
+  local finder = finders.new_table {
+    results = {},
+    entry_maker = function(entry)
+      return {
+        display = entry,
+        ordinal = entry,
+        value = entry,
+      }
+    end,
+  }
+
+  local function update_finder_with_answer(answer)
+    finder:clear()
+    finder:add_entry(answer)
+  end
+
   get_user_question(function(user_question)
     local package_name = vim.g.metacode_ai_package_name or "default_package_name"
     local package_version = vim.g.metacode_ai_package_version or "default_package_version"
 
     local answer = query_metacode_ai(package_name, package_version, user_question)
-
-    local default_opts = {
-      prompt_title = 'MetaCode AI Answer',
-      layout_config = {
-        prompt_position = "top",
-        preview_position = "top",
-        preview_height = 0.5,
-      },
-    }
-    local config_opts = vim.tbl_extend("force", default_opts, opts)
-
-    pickers.new(config_opts, {
-      prompt_title = config_opts.prompt_title,
-      finder = telescope.finders.new_table {
-        results = {answer},
-        entry_maker = telescope.make_entry.gen_from_string(config_opts),
-      },
-      sorter = conf.generic_sorter(opts),
-    }):find()
+    update_finder_with_answer(answer)
   end, opts)
+
+  local default_opts = {
+    prompt_title = 'MetaCode AI Answer',
+    layout_config = {
+      prompt_position = "top",
+      preview_position = "top",
+      preview_height = 0.5,
+    },
+  }
+  local config_opts = vim.tbl_extend("force", default_opts, opts)
+
+  pickers.new(config_opts, {
+    prompt_title = config_opts.prompt_title,
+    finder = finder,
+    sorter = conf.generic_sorter(opts),
+    attach_mappings = function(prompt_bufnr, map)
+      map("i", "<CR>", function() on_select(prompt_bufnr, update_finder_with_answer) end)
+      map("n", "<CR>", function() on_select(prompt_bufnr, update_finder_with_answer) end)
+      return true
+    end,
+  }):find()
 end
 
 return M
